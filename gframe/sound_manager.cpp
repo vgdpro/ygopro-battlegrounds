aa8139c66466b0f3f2978a5a4a73d2e1cdfd406e
mercury233 <me@mercury233.me>
2022-04-13 21:35:58 +0800

    wtf fix 7

--- gframe/sound_manager.cpp
+++ gframe/sound_manager.cpp
@@ -17,7 +17,9 @@ bool SoundManager::Init() {
 	if(!engineSound || !engineMusic) {
 		return false;
 	} else {
+#ifdef IRRKLANG_STATIC
 		irrklang::ikpMP3Init(engineMusic);
+#endif
 		return true;
 	}
 #endif // YGOPRO_USE_IRRKLANG
