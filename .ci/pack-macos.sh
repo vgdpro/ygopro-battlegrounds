7de73d7da43fbc7388ac9858dc18123767aa9af3
purerosefallen <78877@qq.com>
2022-06-25 10:56:44 +0800

    remove macos sound support

--- .ci/pack-macos.sh
+++ .ci/pack-macos.sh
@@ -6,4 +6,4 @@ set -o errexit
 apt update && apt -y install tar git zstd
 git submodule update --init
 mkdir dist replay
-tar --zstd -cf dist/ygopro-$CI_COMMIT_REF_NAME-darwin-$TARGET_LOCALE.tar.zst --exclude='.git*' ygopro.app LICENSE README.md lflist.conf strings.conf system.conf cards.cdb script textures deck single pics replay sound windbot bot bot.conf
+tar --zstd -cf dist/ygopro-$CI_COMMIT_REF_NAME-darwin-$TARGET_LOCALE.tar.zst --exclude='.git*' ygopro.app LICENSE README.md lflist.conf strings.conf system.conf cards.cdb script textures deck single pics replay windbot bot bot.conf
