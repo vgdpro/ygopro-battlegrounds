184c8798d60eee6f9b68d3972003aaf330f1e632
purerosefallen <78877@qq.com>
2025-05-06 09:34:15 +0800

    update ci

--- .ci/assets-locale.sh
+++ .ci/assets-locale.sh
@@ -3,9 +3,12 @@ set -x
 set -o errexit
 # ygopro-database
 apt update && apt -y install wget git libarchive-tools
-git clone --depth=1 https://code.mycard.moe/mycard/ygopro-database
+git clone --depth=1 https://code.moenext.com/mycard/ygopro-database
 cp -rf ./ygopro-database/locales/$TARGET_LOCALE/* .
 # ygopro-images
 mkdir pics
-# skip it in develop branch
-# wget -O - https://cdn01.moecube.com/images/ygopro-images-${TARGET_LOCALE}.zip | bsdtar -C pics -xf -
+if [[ "$CI_COMMIT_REF_NAME" == *"develop"* || "$CI_COMMIT_REF_NAME" == *".pre"* ]]; then
+  echo "This is a pre-release, skipping download."
+else
+  wget -O - https://cdn02.moecube.com:444/images/ygopro-images-${TARGET_LOCALE}.zip | bsdtar -C pics -xf -
+fi
