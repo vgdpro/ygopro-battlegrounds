7326a479b1d5f4ee286b152796b68d426e1c53d7
nanahira <78877@qq.com>
2025-05-09 16:54:35 +0000

    Update .ci/asset-branch, .ci/prepare-irrlicht.sh, .ci/assets-locale.sh, .ci/upload-packager.sh files

--- .ci/assets-locale.sh
+++ .ci/assets-locale.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -x
 set -o errexit
+source .ci/asset-branch
 # ygopro-database
-apt update && apt -y install wget git libarchive-tools
-git clone --depth=1 https://code.moenext.com/mycard/ygopro-database
-cp -rf ./ygopro-database/locales/$TARGET_LOCALE/* .
+apt update && apt -y install wget git libarchive-tools sqlite3
+git clone --depth=1 -b "$ASSET_BRANCH_NAME" https://code.moenext.com/mycard/ygopro-database
+cp -rf ./ygopro-database/locales/$TARGET_LOCALE/strings.conf .
+rm -f cards.cdb
+sqlite3 ./ygopro-database/locales/$TARGET_LOCALE/cards.cdb .dump | sqlite3 cards.cdb
 # ygopro-images
 mkdir pics
-if [[ "$CI_COMMIT_REF_NAME" == *"develop"* || "$CI_COMMIT_REF_NAME" == *".pre"* ]]; then
+if [[ "$ASSET_BRANCH_NAME" == "develop" ]]; then
   echo "This is a pre-release, skipping download."
 else
   wget -O - https://cdn02.moecube.com:444/images/ygopro-images-${TARGET_LOCALE}.zip | bsdtar -C pics -xf -
