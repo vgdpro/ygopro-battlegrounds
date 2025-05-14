9f3a37d414d486458d895424d71476dbedc12cc0
Wind2009-Louse <wind.jackey@qq.com>
2025-05-14 22:34:22 +0800

    sync ci

--- .ci/libevent-prebuild.sh
+++ .ci/libevent-prebuild.sh
@@ -3,7 +3,7 @@ set -x
 set -o errexit
 # PROCESSOR_COUNT=4
 
-wget -O - https://cdn01.moecube.com/ygopro-build-materials/libevent-2.0.22-stable.tar.gz | tar zfx -
+wget -O - https://cdn02.moecube.com:444/ygopro-build-materials/libevent-2.0.22-stable.tar.gz | tar zfx -
 cd libevent-2.0.22-stable
 ./configure --prefix=$PWD/libevent-stable --disable-openssl --enable-static=yes --enable-shared=no
 make -j$PROCESSOR_COUNT
